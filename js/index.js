c1edd6c0b0bc544638003b8242a3232d2490b71e
Olga Adams <helga7777@gmail.com>
2023-03-02 22:03:35 -0700

    added footer with my name, date

--- js/index.js
+++ js/index.js
@@ -1,4 +1,10 @@
 const today = new Date();
 const thisYear = today.getFullYear();
 
+const footer = document.querySelector("footer");
+const copyright = document.createElement("p");
+
+copyright.innerHTML = `Olga Adams ${thisYear}`;
+
+footer.appendChild(copyright);
 
